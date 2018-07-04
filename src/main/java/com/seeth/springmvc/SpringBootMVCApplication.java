92ff4c1c06cd9a1ad79c6c1672500721cfca98c8
seethend <seethendreddy96@gmail.com>
2018-07-04 18:21:17 +0530

    Latest

--- src/main/java/com/seeth/springmvc/SpringBootMVCApplication.java
+++ src/main/java/com/seeth/springmvc/SpringBootMVCApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMvcApplication {
+public class SpringBootMVCApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcApplication.class, args);
+		SpringApplication.run(SpringBootMVCApplication.class, args);
 	}
 }
